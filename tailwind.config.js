def0131cd13f54786134b2047a3bd34c1b6c6d88
broliux <abramikas@gmail.com>
2024-10-18 03:11:30 +0300

    hopefully finished setting up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{svelte,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
